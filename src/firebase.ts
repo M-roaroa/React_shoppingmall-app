41540d65493b19d49ac1f01f6505cf3bb86bf464
갬갬 <roaroa@MacBookAir.local>
2023-11-22 01:47:24 +0900

    commit

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdB3uAAufZVZ5qkXOgzu_W3oPz4aS8Nhw",
+  authDomain: "shoppingmall-app-7caca.firebaseapp.com",
+  projectId: "shoppingmall-app-7caca",
+  storageBucket: "shoppingmall-app-7caca.appspot.com",
+  messagingSenderId: "16962777788",
+  appId: "1:16962777788:web:f83a034f5e499eaaad16b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
